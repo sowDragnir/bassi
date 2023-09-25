e85c319a386e84fe28965c37212658b1768d7cb2
sowDragnir <neybassj@gmail.com>
2023-09-25 18:22:57 +0000

    [commit]zzzz

--- hst/models/models.py
+++ hst/models/models.py
@@ -1,5 +1,5 @@
 from odoo import models, fields, api
-#from odoo import ValidationError
+from odoo.exceptions import ValidationError
 
 
 class hst(models.Model):
@@ -13,7 +13,6 @@ class hst(models.Model):
     is_pacient = fields.Boolean(default=False)
     birthday = fields.Date(string='Birthday')
     other_obs = fields.Char(string='another Observation')
-    #csb = fields.Char(string='gcdg')
 
     @api.model
     def create(self, vals_list):
@@ -22,31 +21,21 @@ class hst(models.Model):
             if vals_list.get('dni'):
                 pacient_with_dni = self.env['res.partner'].search([('dni','=', vals_list['dni']), ('dni', '=', vals_list['dni'].upper())])
                 if pacient_with_dni:
-                    raise ValidationError('DNI already exists')
+                    raise ValidationError('DNI ya existe')
                 else:
                     vals_list['dni'] = vals_list['dni'].upper()
+
+            if vals_list.get('nhc_code', 'new') =='new':
+                    vals_list['nhc_code'] = self.env['ir.sequence'].next_by_code('res.partner') or ('New')
+                    result = super(hst , self).create(vals_list)
+                    return result
+
             
             # if vals_list.get('cip_code'):
             #     #cip_code = vals_list.get('cip_code').upper()
             #     pacient_with_cip_code = self.env['res.partner'].search([('cip_code', '=', vals_list['cip_code']),('cip_code', '=', vals_list['cip_code'].upper())])
             #     if pacient_with_cip_code:
-            #         raise ValidationError('CIP already exists')
+            #         raise ValidationError('CIP already exists') 
             #     else:
             #         vals_list['cip_code'] = vals_list['cip_code'].upper()
-    @api.model_create_multi
-    def create(self, vals_list):
-        if vals_list.get('nhc_code', 'New') == 'New':
-            vals_list['ref'] = self.env['ir.sequence'].next_by_code('res.P')
-            vals_list['sequence_id'] = self.env['ir.sequence'].create().next_by_code()
-
-            return super('nhc_code', self).create(vals_list)
-            
-
-            # vals_list['nhc_code'] = self.env['ir.sequence'].next_by_code('res.partner')
-            # vals_list['sequence_id'] = self.env['ir.sequence'].create({
-            #     'padding': 3,
-            #     'name': vals_list['nhc_code'],
-            #     'prefix': 'EDP.'
-            # }).next_by_code('res.partner')
-            # return super( self).create(vals_list)
-            
+   
---
+++ readme.txt
@@ -0,0 +1,4 @@
+[tail -f /var/log/odoo/odoo.log],,,,,,,,,,,,para ver los logs en terminal
+
+[/mnt/extra-addons
+	/usr/lib/python3/dist-packages/odoo/addons],,,,,,,,,,ruta de acceso a las librerias de odoo
